1a1906c0933d65f37fb8d30ff650f824703f0f85
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2024-10-13 09:20:36 +0100

    added has_loop() exercise

---
+++ linked_list_has_loop.py
@@ -0,0 +1,54 @@
+'''
+    LINKED LIST: has_loop()
+    Algorithm and data structure
+    Solved in Python by Alessandro Silvestri - 2024
+    <alessandro.silvestri.work@gmail.com>
+
+'''
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        while True:
+            slow = slow.next
+            fast = fast.next.next
+            if fast is None or fast.next is None:
+                return False
+            if slow.value == fast.value:
+                return True
+
+
+my_linked_list_1 = LinkedList(1)
+my_linked_list_1.append(2)
+my_linked_list_1.append(3)
+my_linked_list_1.append(4)
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop() ) # Returns True
+
+my_linked_list_2 = LinkedList(1)
+my_linked_list_2.append(2)
+my_linked_list_2.append(3)
+my_linked_list_2.append(4)
+print(my_linked_list_2.has_loop() ) # Returns False
