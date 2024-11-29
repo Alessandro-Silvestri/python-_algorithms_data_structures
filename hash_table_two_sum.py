a8a558b3d3b2ca699a73cbc55d8851ea388247ae
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2024-11-29 12:30:39 +0000

    two_sum pass the all the tests except duplicate values

--- hash_table_two_sum.py
+++ hash_table_two_sum.py
@@ -1,12 +1,42 @@
-def two_sum(ints:list, target:int):
-    dict_numbers = {}
-    for num in ints:
-        dict_numbers[num] = target - num
-    return dict_numbers
+# It pass all the tests but not the duplicate numbers line: print(two_sum([3, 3], 6))
 
+def two_sum(ints:list, target:int):
+    list_result = []
+    dict_numbers = {}
+    index_key = 0
+
+    for num in ints:
     
-    
-    
+
+        dict_numbers[num] = target - num
+
+
+    found = False
+    for key_num in dict_numbers:
+
+        # edge case if number is the half of the target
+        if key_num == target / 2:
+            index_key += 1
+            continue
+        
+        if dict_numbers[key_num] in dict_numbers and found == False:
+            list_result.append(index_key)
+            found = True
+            first_number = key_num    
+
+
+
+        if found:
+            if key_num == target - first_number:
+                list_result.append(index_key)
+                break
+       
+        index_key += 1
+
+
+
+    return list_result
+
     
 # print(two_sum([5, 1, 7, 2, 9, 3], 10))  
 # print(two_sum([4, 2, 11, 7, 6, 3], 9)) 
@@ -17,6 +47,7 @@ def two_sum(ints:list, target:int):
 # print ( two_sum([1, 2, 3, 4, 5], 3) )
 # print ( two_sum([], 0) )
 
+print(two_sum([3, 3], 6))
 
 
 """
@@ -35,5 +66,6 @@ def two_sum(ints:list, target:int):
 
 
 # O(1)
-# my_dict = {"a": 1, "b": 2, "c": 3, "d": 4}
-# print("d" in my_dict)
+# my_dict = {"a": 1, "b": 2, "c": 3, "d": 4, "a": 3}
+# print("a" in my_dict)
+
