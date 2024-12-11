d6099995a0a4417b582e046123de9232616adbad
Alessandro Silvestri <alessandro.silvestri.work@gmail.com>
2024-12-11 09:50:26 +0000

    finished the graph section

--- graph.py
+++ graph.py
@@ -1,3 +1,10 @@
+'''
+    GRAPH
+    Algorithm and data structure
+    Solved in Python by Alessandro Silvestri - 2024
+    <alessandro.silvestri.work@gmail.com>
+'''
+
 class Graph:
     def __init__(self):
         self.adj_list = {}
@@ -15,42 +22,45 @@ class Graph:
     
     def add_edge(self, v1, v2):
         '''bidirectional connection'''
-        # edge case
+        # edge case: if the vertexes are not present in the graph
         if not v1 in self.adj_list or not v2 in self.adj_list or v1 == v2:
             return False
-
         # vertexes connection
-        if not v2 in self.adj_list[v1] and not v1 in self.adj_list[v2]:
+        if not v2 in self.adj_list[v1]:
             self.adj_list[v1].append(v2)
             self.adj_list[v2].append(v1)
             return True
 
-        
-        
+    def remove_edge(self, v1, v2):
+        # edge case: if the vertexes are not present in the graph
+        if not v1 in self.adj_list or not v2 in self.adj_list or v1 == v2:
+            return False     
+        if v2 in self.adj_list[v1]:
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+            return True
+    
+    def remove_vertex(self, v):
+        if not v in self.adj_list:
+            return False
+        for i in self.adj_list[v]:
+            self.adj_list[i].remove(v)
+        self.adj_list.pop(v)
+        return True
+
 
 my_graph = Graph()
-
 my_graph.add_vertex("A")
 my_graph.add_vertex("B")
 my_graph.add_vertex("C")
 my_graph.add_vertex("D")
-my_graph.print_graph()
-
-my_graph.add_edge("A", "D")
-my_graph.print_graph()
-my_graph.add_edge("B", "C")
-my_graph.print_graph()
+my_graph.add_edge("A", "B")
 my_graph.add_edge("B", "D")
+my_graph.add_edge("D", "C")
+my_graph.add_edge("A", "C")
+my_graph.add_edge("A", "D")
+my_graph.remove_vertex("D")
 my_graph.print_graph()
-my_graph.add_vertex("H")
-my_graph.add_edge("H", "B")
-my_graph.add_edge("H", "A")
-my_graph.print_graph()
-my_graph.add_edge("C", "H")
-my_graph.print_graph()
-
-
-
 
 
 
